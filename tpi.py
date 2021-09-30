80e563c3e7ab8453269bfa7ece7665c7ce5af259
Juan Cruz Soto <juancruzlp2013@gmail.com>
2021-09-30 14:21:04 -0300

    function calculate entropy created

--- tpi.py
+++ tpi.py
@@ -1,7 +1,9 @@
 #Convert csv to list by stackoverflow
 import csv
+import math
+
 tabla = []
-with open("./prueba.csv") as csvfile:
+with open("./prueba2.csv") as csvfile:
     reader = csv.reader(csvfile) # change contents to floats
     for row in reader: # each row is a list
         tabla.append(row)
@@ -18,6 +20,7 @@ list=[] #lista que resguarda las variables existentes en el conjunto D
 cont=[] #Lista que contabiliza las apariciones de las variables del conjunto D
 #Recorre ultima fila (salteando el encabezado de la misma) para contabilizar la cantidad de apariciones de las variables del conjunto D
 for i in range(1,len(tabla)):
+    print(tabla[i][len(tabla[1])-3])
     if (tabla[i][len(tabla[1])-1]) in list:
         aux = list.index(tabla[i][len(tabla[1])-1]) #Posicion de la variable del conjunto D
         cont[aux]=cont[aux]+1 #Contabilizo la variable en una segunda lista (cont)
@@ -26,7 +29,6 @@ for i in range(1,len(tabla)):
         cont.append(1) #Agrego una posicion a la lista "cont" inicializada en 1, por la aparición de la variable
 print(list)
 print(cont)
-
 
 def form(a,b): 
     import math
@@ -42,4 +44,34 @@ def calcEntropy(listCant):
         entropy=entropy+form(listCant[i],tot)
     print("La entropia resultante es: "+str(entropy))
 
+def calcEntropyAtr(table):
+    entropys = {} #Diccionario donde se cargaran las entropias de cada Atributo
+
+    columnas=len(table[1])-1 #se le resta 1 para no incluir la columna de la clase
+    for x in range(1,columnas): #Recorreremos la tabla por columnas para trabajar con cada Atributo por separado, salteamos la primera columna de ID
+        valores = {} #Diccionario donde se cargaran los valores necesarios para calcular la entropia del atributo
+        for y in range(1,len(table)): #Para recorrer los valores de la columna del atributo en la que estamos posicionados, salteamos la primera fila que es la cabecera
+            campo = table[y][x] #Se toma el valor que toma el atributo en esa fila
+            if campo in valores: #Pregunta si ya se analizo este valor para inicializarlo a 1 o incrementarlo
+                if table[y][columnas] in valores[campo]: #Se consulta si el valor de clase en esa fila ya fue analizado con ese atributo para inicializarlo en 1 o incrementarlo.
+                    valores[campo][table[y][columnas]]+=1
+                else:
+                    valores[campo][table[y][columnas]]=1
+                valores[campo]["cantidad"]+=1
+            else:
+                valores[campo]={"cantidad":1}           
+                valores[campo][table[y][columnas]]=1 
+        entropys[table[0][x]]=0
+        for idx in valores:
+            cantidad=valores[idx]["cantidad"]
+            valores[idx].pop("cantidad")
+            variablesFuncion=[]
+            for var in valores[idx]:
+                entropys[table[0][x]]+=cantidad/(len(table)-1) * (-valores[idx][var]/cantidad * math.log(valores[idx][var]/cantidad, 2))
+        entropys[table[0][x]]=round(entropys[table[0][x]], 3)
+    print("Entropia de las variables: ",entropys)
+
+
+
 calcEntropy(cont)
+calcEntropyAtr(tabla)
