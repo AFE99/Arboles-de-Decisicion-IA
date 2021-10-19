254cc24ae3d1d6ac6cc2cb0414834eead44438fc
juancruzsoto <juancruzlp2013@gmail.com>
2021-10-18 23:14:08 -0300

    Graphic Interface created

--- tpi.py
+++ tpi.py
@@ -3,15 +3,17 @@ import csv
 import math
 import copy
 from pprint import pprint
+from tkinter import ttk
+import tkinter as tk
 class GenerarArbol:
     def __init__(self,path):
-        
+
         self.tabla = []  #Tabla original con todos los atributos y clases
         self.entropyD = 0
         self.entropysAtr = {}
         self.Tree = []
         self.NodeSheet = ""
-        self.NodeParent = {"parent":None,"branch":None}        
+        self.NodeParent = {"parent":None,"branch":None}
 
         with open(path) as csvfile:
             reader = csv.reader(csvfile) # change contents to floats
@@ -35,15 +37,15 @@ class GenerarArbol:
         # fmt = '\t'.join('{{:{}}}'.format(x) for x in lens)
         # self.tablaPrint = [fmt.format(*row) for row in s]
         # print('\n'.join(self.tablaPrint))
- 
+
     def prettyPrint(self,table):
         s = [[str(e) for e in row] for row in table]
         lens = [max(map(len, col)) for col in zip(*s)]
         fmt = '\t'.join('{{:{}}}'.format(x) for x in lens)
         table = [fmt.format(*row) for row in s]
         print('\n'.join(table))
-    
-    #Método que contabiliza la cantidad de apariciones del conjunto D 
+
+    #Método que contabiliza la cantidad de apariciones del conjunto D
     def varConjD(self,Tabla):
         #Inicializo listas
         list=[] #lista que resguarda las variables existentes en el conjunto D
@@ -60,7 +62,7 @@ class GenerarArbol:
         # print(cont)
         return cont
 
-    def form(self,a,b): 
+    def form(self,a,b):
         import math
         c = -((a/b)*math.log(a/b,2))
         return(c)
@@ -96,8 +98,8 @@ class GenerarArbol:
                         valores[campo][Tabla[y][columnas]]=1
                     valores[campo]["cantidad"]+=1
                 else:
-                    valores[campo]={"cantidad":1}           
-                    valores[campo][Tabla[y][columnas]]=1 
+                    valores[campo]={"cantidad":1}
+                    valores[campo][Tabla[y][columnas]]=1
             entropysAtr[Tabla[0][Atr]]={"entropia":0,"Djs":[],"Vars":[]}
             pprint(valores, width=1)
             for idx in valores:
@@ -106,7 +108,7 @@ class GenerarArbol:
                         entropysAtr[Tabla[0][Atr]]["Vars"].append(idx)
                 cantidad=valores[idx]["cantidad"]
                 valores[idx].pop("cantidad")
-                
+
                 entropysAtr[Tabla[0][Atr]]["Djs"].append(cantidad)
                 for var in valores[idx]:
                     #print(var,valores[idx],valores[idx][var])
@@ -128,12 +130,12 @@ class GenerarArbol:
             if ganAux > ganancia:
                 ganancia = ganAux
                 NodoG=e
-            
+
             for Dj in entropysAtr[e]["Djs"]:
                 tasaAux+=-Dj/(len(Tabla)-1) * math.log(Dj/(len(Tabla)-1), 2)
             # print(ganAux,tasaAux,"pri")
             tasaAux=ganAux/tasaAux
-            
+
             if tasaAux > Tasaganancia:
                 Tasaganancia = tasaAux
                 NodoTG=e
@@ -152,7 +154,7 @@ class GenerarArbol:
             #print(contador,rows,Tabla[x][column],Variable)
             if Tabla[x][column] == Variable:
                 tablaTemp2.append(Tabla[x])
-        
+
         return tablaTemp2
 
     def ExamplesSameClass(self,Tabla):
@@ -183,11 +185,11 @@ class GenerarArbol:
             if valores[x]>marca:
                 marca = valores[x]
                 valorMasFreq = x
-        
+
         return valorMasFreq
 
     def AlgoritmoC45(self,Tabla,Atributes,Tree):
-        
+
         if self.ExamplesSameClass(Tabla):
             Tree.append(self.NodeSheet)
             # Tree.append(Node(self.NodeSheet,self.NodeParent["parent"]))
@@ -195,7 +197,7 @@ class GenerarArbol:
         elif len(Atributes) == 0:
             print("No hay mas atributos a analizar, tabla= ", pprint(Tabla, width=1))
             # print(Tabla)
-            
+
             Tree.append(self.ValorMasFreq(Tabla))
             # for x in Tree:
             #     print(x.getName())
@@ -219,7 +221,7 @@ class GenerarArbol:
                 #     self.NodeParent["parent"].setChild(Tree[len(Tree)-1],self.NodeParent["branch"])
                 # self.NodeParent["parent"] = Tree[len(Tree)-1]
                 # print(self.NodeParent["parent"].getChild())
-                
+
                 #print(self.tabla)
                 Dpartition = []
                 # print(EntropysAtr[Ag]["Vars"])
@@ -231,24 +233,24 @@ class GenerarArbol:
                         print(Dpartition[i][j])
                 # print(Dpartition)
                 # print(Ag,Atributes)
-                
+
                 Atributes2 = copy.deepcopy(Atributes)
-                Atributes2.remove(Ag)                 
-        
+                Atributes2.remove(Ag)
+
                 for Dj in range(len(Dpartition)):
                     Tree.append(EntropysAtr[Ag]["Vars"][Dj])
                     # self.NodeParent["branch"] = EntropysAtr[Ag]["Vars"][Dj]
                     # print(self.NodeParent["branch"])
                     # input()
                     # Tree[len(Tree)-1].setChild(None,EntropysAtr[Ag]["Vars"][Dj])
-                    
+
                     Tabla = Dpartition[Dj]
                     # print("ENTREEE")
                     self.AlgoritmoC45(Tabla,Atributes2,Tree)
-                
+
                 #print(Tree[0].getChild())
         print("\nArbol: ",Tree)
-        print("\n Los atributos identificados son: ",Atributes,len(Atributes))           
+        print("\n Los atributos identificados son: ",Atributes,len(Atributes))
 
 
 
@@ -269,19 +271,62 @@ class Node:
 
     def getParent(self):
         return self.parent
-    
+
     def setChild(self,child,branch):
         self.child[branch] = child
-    
+
     def getChild(self):
         return self.child
 
 
+class GraphicInterface:
+    def __init__(self, master,tk):
+        self.master = master
+        self.master.title("TPI Inteligencia Artificial - Grupo 9")
+		# self.master.iconbitmap()
+        self.master.resizable(0,0)
+        self.master.geometry("550x350")
+        self.frameInicio = tk.Frame(self.master, width=400, height=250,bg="#9BBCD1",relief="groove", borderwidth=5)
+        self.frameInicio.pack(fill="both",side="top",expand="YES")
 
+        self.Titulo=tk.Label(self.frameInicio,bg="#9BBCD1",text="TPI - Inteligencia Artificial - Grupo 9",fg="black",height=2,font=("Ubuntu",22))
 
+        self.Integrantes = tk.Label(self.frameInicio,bg="#9BBCD1",text="Integrantes:",fg="black",height=2,font=("Ubuntu",15))
+        self.Integrante1 = tk.Label(self.frameInicio,bg="#9BBCD1",text="Acevedo, Fernando",fg="black",height=1,font=("Ubuntu",10))
+        self.Integrante2= tk.Label(self.frameInicio,bg="#9BBCD1",text="Figueroa, Simon",fg="black",height=1,font=("Ubuntu",10))
+        self.Integrante3 = tk.Label(self.frameInicio,bg="#9BBCD1",text="Ortiz, Claudia",fg="black",height=1,font=("Ubuntu",10))
+        self.Integrante4 = tk.Label(self.frameInicio,bg="#9BBCD1",text="Soto, Juan Cruz",fg="black",height=1,font=("Ubuntu",10))
+        self.botonIniciar=tk.Button(self.frameInicio, text="Iniciar",bg="#2D373D",fg="#42D5FF",width=10,height=0,command=lambda:self.MenuPrincipal())
 
-# calcEntropy(cont)
-# calcEntropyAtr(tabla)
+        self.Titulo.grid(row=0,column=0,columnspan=5,padx=20,pady=10)
+        self.Integrantes.grid(row=1,column=1,columnspan=3)
+        self.Integrante1.grid(row=2,column=1,columnspan=3)
+        self.Integrante2.grid(row=3,column=1,columnspan=3)
+        self.Integrante3.grid(row=4,column=1,columnspan=3)
+        self.Integrante4.grid(row=5,column=1,columnspan=3)
+        self.botonIniciar.grid(row=6,column=2,padx=20,pady=20)
+
+    def MenuPrincipal(self):
+        self.raiz=tk.Toplevel(self.master)
+        #self.raiz.focus_set()
+        #self.raiz.grab_set()
+        self.raiz.title("TPI Inteligencia Artificial - Grupo 9")
+        self.raiz.resizable(0,0)
+        self.raiz.geometry("800x550+300+40")
+        self.raiz.config(bg="#9BBCD1")
+
+        self.miFrame1=tk.Frame(self.raiz, width=800, height=100,bg="#9BBCD1",relief="groove", borderwidth=5)
+        self.miFrame1.pack(fill="both",side="top")
+        self.miFrame2=tk.Frame(self.raiz, width=800, height=450,bg="#9BBCD1",relief="groove", borderwidth=5)
+        self.miFrame2.pack(fill="both",side="top",expand="YES")
+
+        self.TituloMenu=tk.Label(self.miFrame1,bg="#9BBCD1", text="Generar Arboles de Decisión - Algoritmo C4.5",fg="#323638",font=("Ubuntu",25))
+        self.TituloMenu.pack(fill="both",side="top")
+
 if __name__ == "__main__":
+    raizMaster = tk.Tk()
+    Programa = GraphicInterface(raizMaster,tk)
+    raizMaster.mainloop()
+    
     Arbol = GenerarArbol("./prueba4.csv")
     Arbol.AlgoritmoC45(Arbol.tabla,Arbol.Atributes,Arbol.Tree)
